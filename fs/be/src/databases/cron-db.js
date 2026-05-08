9e97ffc3bcd90551f6df2ef81a894bde6a62e10c
NofaFirdaus <nofafirdaus17@gmail.com>
2026-05-09 00:17:31 +0700

    fix:cron job

--- fs/be/src/databases/cron-db.js
+++ fs/be/src/databases/cron-db.js
@@ -3,25 +3,26 @@ import DatabasePool from './database-pool.js';
 
 const db =new DatabasePool;
 cron.schedule('*/5 * * * *', async () => {
-  await db.pool.query(`
-    DELETE FROM verifikasi_email
-    WHERE "expired_at" < NOW()
-  `);
-});
+  try {
+    await Promise.all([
+      db.pool.query(`
+        DELETE FROM verifikasi_email
+        WHERE expired_at < NOW()
+      `),
 
-cron.schedule('*/5 * * * *', async () => {
-  await db.pool.query(`
-    DELETE FROM reset_password
-    WHERE "expired_at" < NOW()
-  `);
-});
+      db.pool.query(`
+        DELETE FROM reset_password
+        WHERE expired_at < NOW()
+      `),
 
+      db.pool.query(`
+        DELETE FROM authentications
+        WHERE expired_at < NOW()
+      `)
+    ]);
 
-cron.schedule('*/5 * * * *', async () => {
-  await db.pool.query(`
-      DELETE FROM authentications
-      WHERE expired_at < NOW()
-      RETURNING id
-    `);
-
+    console.log('Cleanup success');
+  } catch (err) {
+    console.error('Cron cleanup error:', err);
+  }
 });
