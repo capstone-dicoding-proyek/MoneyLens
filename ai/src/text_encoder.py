e90826aa84d1a9c0efb04e28662d41a17b588c0c
remiahasan <hasan@a.localdomain>
2026-05-27 20:38:47 +0700

    training v3.0

--- ai/src/text_encoder.py
+++ ai/src/text_encoder.py
@@ -7,21 +7,25 @@ char_to_num = tf.keras.layers.StringLookup(
     num_oov_indices=1
 )
 
+_vocab = [v for v in char_to_num.get_vocabulary() if v != "[UNK]"]
+
 num_to_char = tf.keras.layers.StringLookup(
-    vocabulary=char_to_num.get_vocabulary(),
+    vocabulary=_vocab,
     mask_token=None,
     num_oov_indices=1,
     invert=True
 )
 
 def encode_text(text):
-    text = tf.strings.unicode_split(
-        text,
-        input_encoding="UTF-8"
-    )
+    """Encode string ke sequence of integer indices (1-based, 0=blank)."""
+    text = tf.strings.unicode_split(text, input_encoding="UTF-8")
     return char_to_num(text)
 
 def decode_prediction(pred):
+    """
+    Decode output logits model ke string.
+    pred: tensor shape (batch, time_steps, num_classes)
+    """
     pred = tf.nn.softmax(pred, axis=-1)
 
     input_len = tf.ones(pred.shape[0]) * pred.shape[1]
@@ -33,11 +37,9 @@ def decode_prediction(pred):
     )[0][0]
 
     output_text = []
-
     for res in results:
         res = num_to_char(res)
         res = tf.strings.reduce_join(res).numpy().decode("utf-8")
-        # Hapus token [UNK] (blank/padding)
         res = res.replace("[UNK]", "")
         res = " ".join(res.split())
         output_text.append(res)
