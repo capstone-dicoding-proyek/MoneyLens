72c159c2942605791a116336355511b04f3ebef5
NofaFirdaus <nofafirdaus17@gmail.com>
2026-09-02 21:59:44 +0700

    fix db pool

--- fs/be/src/databases/database-pool.js
+++ fs/be/src/databases/database-pool.js
@@ -4,11 +4,7 @@ import { Pool } from 'pg';
 class DatabasePool {
   constructor() {
     this.pool = new Pool({
-      host: process.env.PGHOST,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE,
-      port: process.env.PGPORT,
+      connectionString: process.env.DATABASE_URL,
     });
   }
 }
